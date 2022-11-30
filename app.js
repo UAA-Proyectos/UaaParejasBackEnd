3aed197d8aef963681e554601992d5a324819fff
Victor-Zaragoza <zaragoza_victorr@outlook.com>
2022-11-30 00:10:55 -0600

    Habilitando http reponses

--- app.js
+++ app.js
@@ -2,21 +2,30 @@
 const express = require('express');
 const app = express();
 
+app.all('/*', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+const multer = require('multer');
+const path = require('path');
+
 //2 - Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());//además le decimos a express que vamos a usar json
 
 //3- Invocamos a dotenv
 const dotenv = require('dotenv');
-dotenv.config({ path: './env/.env'});
+dotenv.config({ path: './env/.env' });
 
 //4 -seteamos el directorio de assets
-app.use('/resources',express.static('public'));
+app.use('/resources', express.static('public'));
 app.use('/resources', express.static(__dirname + '/public'));
 
 //5 - Establecemos el motor de plantillas
-app.set('view engine','ejs');
-
+app.set('view engine', 'ejs');
+  
 //6 -Invocamos a bcrypt
 const bcrypt = require('bcryptjs');
 
@@ -33,78 +42,80 @@ app.use(session({
 const connection = require('./database/db');
 
 //9 - establecemos las rutas
-	app.get('/login',(req, res)=>{
-		res.render('login');
-	})
+app.get('/login', (req, res) => {
+	res.render('login');
+});
 
-	app.get('/register',(req, res)=>{
-		res.render('register');
-	})
+app.get('/register', (req, res) => {
+	res.render('register');
+})
 
 //10 - Método para la REGISTRACIÓN
-app.post('/register', async (req, res)=>{
+app.post('/register', async (req, res) => {
 	const user = req.body.user;
-    const id = req.body.id;
     const pass = req.body.pass;
-    const date = req.body.date;
-    const email = req.body.email;
+	const date = req.body.date;
+	const email = req.body.email;
 	let passwordHash = await bcrypt.hash(pass, 8);
-    connection.query('INSERT INTO user SET ?',{id:id, username:user, birthdate:date, email:email,password:passwordHash}, async (error, results)=>{
-        if(error){
-            console.log(error);
-        }else{            
-			res.render('register', {
-				alert: true,
-				alertTitle: "Registration",
-				alertMessage: "¡Successful Registration!",
-				alertIcon:'success',
-				showConfirmButton: false,
-				timer: 1500,
-				ruta: ''
-			});
-            //res.redirect('/');         
-        }
+	
+    connection.query('INSERT INTO user SET ?', { username: user, birthdate: date, email: email, password: passwordHash }, async (error, results) => {
+		if (error) {
+			console.log(error);
+			if (error.code = "ER_DUP_ENTRY") {
+				res.status(409)
+				res.send();
+			}
+			else {
+				res.status(500)
+				res.send()
+			}
+		} else {
+			res.status(201)
+			res.send()
+		}
 	});
 })
 
 
 
 //11 - Metodo para la autenticacion
-app.post('/auth', async (req, res)=> {
-	const user = req.body.user;
-	const pass = req.body.pass;    
-    let passwordHash = await bcrypt.hash(pass, 8);
+app.post('/auth', async (req, res) => {
+	const user = req.body.email;
+	const pass = req.body.password;
 	if (user && pass) {
-		connection.query('SELECT * FROM user WHERE username = ?', [user], async (error, results, fields)=> {
-			if( results.length == 0/*!(await bcrypt.compare(pass, results[0].pass)*/) {    
-				res.render('login', {
-                        alert: true,
-                        alertTitle: "Error",
-                        alertMessage: "USUARIO y/o PASSWORD incorrectas",
-                        alertIcon:'error',
-                        showConfirmButton: true,
-                        timer: false,
-                        ruta: 'login'    
-                    });
-				
-				//Mensaje simple y poco vistoso
-                //res.send('Incorrect Username and/or Password!');				
-			} else {         
-				//creamos una var de session y le asignamos true si INICIO SESSION       
-				req.session.loggedin = true;                
-				req.session.username = results[0].username;
-				res.render('login', {
-					alert: true,
-					alertTitle: "Conexión exitosa",
-					alertMessage: "¡LOGIN CORRECTO!",
-					alertIcon:'success',
-					showConfirmButton: false,
-					timer: 1500,
-					ruta: ''
-				});        			
-			}			
-			res.end();
-		});
+		let passwordHash = await bcrypt.hash(pass, 8);
+		connection.query(`SELECT * FROM user WHERE email = '${user}'`, async (error, results, fields) => {
+        
+        if (error) {
+            console.log(error, 'errs');
+        }
+        console.log(passwordHash);
+    
+        if (results.length > 0) {
+            const match = await bcrypt.compare(pass, results[0].password)
+            //creamos una var de session y le asignamos true si INICIO SESSION   
+            req.session.loggedin = true;                
+            req.session.username = results[0].username;
+            if (match) {
+                res.send({
+                    id: results[0].id
+                })
+            }
+            else {
+                res.status(401);
+                res.send({
+                    message: 'Data not found'
+                })
+            }
+        } else {
+            res.status(404);
+            res.send({
+                message: 'Data not found'
+            })
+        }
+        res.end();
+    });
+			
 	} else {	
 		res.send('Please enter user and Password!');
 		res.end();
@@ -112,34 +123,34 @@ app.post('/auth', async (req, res)=> {
 });
 
 //12 - Método para controlar que está auth en todas las páginas
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
 	if (req.session.loggedin) {
-		res.render('index',{
+		res.render('index', {
 			login: true,
-			name: req.session.username			
-		});		
-	} else {
-		res.render('index',{
-			login:false,
-			name:'Debe iniciar sesión',			
-		});				
+            name: req.session.username
+		});
+	} else {	
+		res.render('index', {
+			login: false,
+			name: 'Debe iniciar sesión',
+		});
 	}
 	res.end();
 });
 
 
 //función para limpiar la caché luego del logout
-app.use(function(req, res, next) {
-    if (!req.user)
-        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    next();
+app.use(function (req, res, next) {
+	if (!req.user)
+		res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+	next();
 });
 
  //Logout
 //Destruye la sesión.
 app.get('/logout', function (req, res) {
 	req.session.destroy(() => {
-	  res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
+        res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
 	})
 });
 
@@ -147,6 +158,7 @@ app.get('/logout', function (req, res) {
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
+const { ok } = require('assert');
 
 // const app = express();
 
@@ -157,21 +169,16 @@ app.use(bodyparser.json());
 // EndPoint Obtener info de un usuario
 app.get('/user/:id', (req, res) => {
     let gID = req.params.id;
-    let query = `select * from user where id = ${gID}`;
+    let query = `select u.birthdate, u.created_at, u.description, u.zodiac_sign, u.username, u.email, ine.name as "interes" from user u inner join user_interest i on (u.id = i.id_user && u.id = ${gID}) inner join interest ine on ine.id = i.category_id`;
     connection.query(query,(err,result) =>{
         if(err){
             console.log(err,'errs');
         }
         if(result.length>0){
-            res.send({
-                message:'1 User data',
-                data: result
-            })
+           res.status(200).send({status: "ok", message: "1 User data", data: result});
         }
         else{
-            res.send({
-                message: 'Data not found'
-            })
+            res.status(400).send('Data not found');
         }
     })
 }) 
@@ -202,6 +209,72 @@ app.put('/user/:id', (req,res) => {
         })
     })
 })
+
+//Cambio de contraseña
+
+app.put('/changepass/:id', (req, res) => {
+    console.log('Edit contraseña');
+    
+    let gID = req.params.id;
+    let oldPassword = req.body.oldPassword;
+    let newPassword = req.body.newPassword;
+
+   
+        let query = `update user set password = '${newPassword}' where id = ${gID}`;
+        connection.query(query, (err, result) =>{
+            if(err){
+                console.log(err);
+            }
+            if(oldPassword == "sacar contraseña de la bd")
+                res.status(201).send({status: "ok", message: "data updated"});
+            
+            else
+                res.status(400).send('Error contraseña');
+        });
+    
+        
+})
+
+//Ruta donde almacenamos las imagenes
+
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+const storage = multer.diskStorage({
+	destination:(req,file,callback) =>{
+		callback(null, 'uploads')
+	},
+	filename: (req, file, callback) =>{
+		callback(null, file.originalname)
+	}
+});
+
+const upload = multer({
+	// storage
+});
+
+app.post('/file',upload.single('file'),(req,res,next) => {
+	const file = req.file;
+
+    const filesImg = {
+
+        id: null,
+        nombre: file.filename,
+        path: file.path,
+        
+    }
+
+    if (!file) {
+        const error = new Error('No File')
+        error.httpStatusCode = 400;
+        return next(error)
+	}
+
+    res.send(file);
+    console.log(filesImg);
+
+    mysqlConnection.query('INSERT INTO files set ?', [filesImg]);
+})
+
+
 
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING IN http://localhost:3000');
